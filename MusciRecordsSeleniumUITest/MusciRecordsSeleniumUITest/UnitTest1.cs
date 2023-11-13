b4e0b7e510439e3da2fae9051b4a434efd266a6b
Christian Robbers Rasmussen <crr@likehero.dk>
2023-11-13 09:54:50 +0100

    Byggede wedriveren og gør klar til tests med selenium

---
+++ MusciRecordsSeleniumUITest/MusciRecordsSeleniumUITest/UnitTest1.cs
@@ -0,0 +1,36 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace MusciRecordsSeleniumUITest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private static readonly string DriverDir = "C:\\Webdrivers";
+        private static IWebDriver driver;
+
+        // Initialisere webdriveren
+        [ClassInitialize]
+
+        public static void Setup(TestContext testContext)
+        {
+            driver = new ChromeDriver(DriverDir);
+        }
+
+        // Rydder ordenligt op efter driveren er initaliseret 
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+          
+        }
+    }
+}
